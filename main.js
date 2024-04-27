af1a04fa1bcab8075b5ea8d2c1018f99360fadb2
axellse <mail@axell.me>
2024-04-27 17:42:58 +0200

    fix that thing

--- main.js
+++ main.js
@@ -12,41 +12,40 @@ let realdirname = __dirname.replaceAll("\\", "/").replace("resources/app.asar", 
 if (process.platform === "win32") { 
   realdirname = realdirname.replaceAll("/", "\\")
 }
-
-
-
-if (process.argv[1] == "uninstall") {
-  app.whenReady().then(() => {
-    UninstallFromLocation(realdirname, true)
-  })
-} else if (__dirname.includes("app.asar") || fs.existsSync(realdirname + "/resources")) {
-  const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 1200,
-      height: 800,
-      autoHideMenuBar: true,
-      fullscreen: true,
-      icon: __dirname + "/build/icon.png",
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
+  if (process.argv[1] == "uninstall") {
+    app.whenReady().then(() => {
+      UninstallFromLocation(realdirname, true)
     })
-    win.loadURL('http://localhost:3000')
-  win.setMenuBarVisibility(null)
-    Menu.setApplicationMenu(null)
+  } else if (process.argv[1] == "portable") {
+    console.log("First launch: Initalizing as portable")
+    AppDataHandler.CreateInstall(__dirname, "Portable")
+    app.relaunch()
+    app.exit()
+  } else {
+  if (__dirname.includes("app.asar") || fs.existsSync(realdirname + "/resources")) {
+    const createWindow = () => {
+      const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        autoHideMenuBar: true,
+        fullscreen: true,
+        icon: __dirname + "/build/icon.png",
+        webPreferences: {
+          preload: path.join(__dirname, 'preload.js')
+        }
+      })
+      win.loadURL('http://localhost:3000')
+      win.setMenuBarVisibility(null)
+      Menu.setApplicationMenu(null)
+    }
+    app.whenReady().then(() => {
+      createWindow()
+      IPC.OnReady()
+    })
+  } else {
+    console.log("First launch: Initalizing as Installed")
+    app.whenReady().then(() => {
+      Installer.StartInstaller()
+    })
   }
-  app.whenReady().then(() => {
-    createWindow()
-    IPC.OnReady()
-  })
-} else if (process.argv[1] == "portable") {
-  console.log("First launch: Initalizing as portable")
-  AppDataHandler.CreateInstall(__dirname, "Portable")
-  app.relaunch()
-  app.exit()
-} else {
-  console.log("First launch: Initalizing as Installed")
-  app.whenReady().then(() => {
-    Installer.StartInstaller()
-  })
 }
