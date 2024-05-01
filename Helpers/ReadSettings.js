45a413bb718e6d6c0b129a3684298bee9ec2b5a6
axellse <mail@axell.me>
2024-05-01 21:43:12 +0200

    fix readsettings installer bug

--- Helpers/ReadSettings.js
+++ Helpers/ReadSettings.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
+let Settings = {}
 let InstallLocation = __dirname.replaceAll("\\", "/").replace("resources/app.asar", "");
 
-const SettingsContent = fs.readFileSync(InstallLocation + "/Settings.json");
-const Settings = JSON.parse(SettingsContent)
+if (!process.env.PORTABLE_EXECUTABLE_FILE && process.argv[1] == "installer") {
+    const SettingsContent = fs.readFileSync(InstallLocation + "/Settings.json");
+    Settings = JSON.parse(SettingsContent)
+}
 
 module.exports = {Settings}
