1c15ba15d534bcd56f8c2ff35ff1e9348fb4283b
axellse <mail@axell.me>
2024-05-04 09:15:45 +0200

    fix the same bug again

--- Helpers/ReadSettings.js
+++ Helpers/ReadSettings.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 let Settings = {}
 let InstallLocation = __dirname.replaceAll("\\", "/").replace("resources/app.asar", "");
 
-if (!process.env.PORTABLE_EXECUTABLE_FILE && process.argv[1] == "installer") {
+if (!process.env.PORTABLE_EXECUTABLE_FILE && process.argv[1] != "installer") {
     const SettingsContent = fs.readFileSync(InstallLocation + "/Settings.json");
     Settings = JSON.parse(SettingsContent)
 }
